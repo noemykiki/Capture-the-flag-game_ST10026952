ff665588260d90c1a613c75ffad35e4b70cf6cf2
Kiki <108219879+noemykiki@users.noreply.github.com>
2024-03-22 12:04:19 +0200

    enemy is picking up flag and carrying it to podium

--- Assets/Scripts/AIController1.cs
+++ Assets/Scripts/AIController1.cs
@@ -9,11 +9,18 @@ public class AIController : MonoBehaviour
     public string flagTag = "RedFlag"; // Tag of the flag the AI is searching for
     public Transform redFlag; 
     private NavMeshAgent agent;
+    private GameObject redFlagObject;
+    public Transform enemyFlagHolder;
+    public Transform redPodium; 
+
+    private bool isCarryingFlag = false;
+    private Vector3 flagOffset = new Vector3(0f, 1f, 0f); // Adjust this offset as needed
 
     private GameObject targetFlag; // Reference to the target flag GameObject
 
     void Start()
     {
+        redFlagObject = GameObject.FindGameObjectWithTag("RedFlag");
         agent = GetComponent<NavMeshAgent>();
         // Find the GameObject with the specified tag
         targetFlag = GameObject.FindGameObjectWithTag(flagTag);
@@ -22,10 +29,37 @@ public class AIController : MonoBehaviour
             Debug.LogError("No GameObject found with tag " + flagTag);
         }
     }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == redFlagObject && !isCarryingFlag)
+        {
+            // Pick up the flag
+            //blueFlag.GetComponent<Rigidbody>().isKinematic = true;
+            redFlagObject.transform.SetParent(enemyFlagHolder);
+            redFlagObject.transform.localPosition = Vector3.zero; // Set local position to (0, 0, 0)
+            isCarryingFlag = true;
+        }
+
+        /**if (other.gameObject == bluePodium && isCarryingFlag)
+        {
+            winUI.gameObject.SetActive(true);
+            isOnBluePodium = true;
+            blueFlag.SetActive(false);
+            redFlag.SetActive(false);
+           
+        }**/
+    }
 
     void Update()
     {
-        if (targetFlag != null)
+        if (isCarryingFlag)
+        {
+            // Update flag position relative to the flag holder
+            redFlag.transform.position = enemyFlagHolder.position + flagOffset;
+            agent.destination = redPodium.position;
+        }
+        if (targetFlag != null && isCarryingFlag == false)
         {
             agent.destination = redFlag.position;
 
